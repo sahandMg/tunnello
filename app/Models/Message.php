b0e82758234ba550f8fb18fb0547889adc0f43b2
Sahand_MG <s23.moghadam@gmail.com>
2022-03-15 17:41:42 +0330

    message body encryption bug fixed

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,10 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    use HasFactory, HasFactory, EncryptableDbAttribute;
+    use HasFactory, HasFactory;
 
     protected $fillable = ['body', 'recipient_id', 'sender_id', 'group_id'];
-    protected $encryptable = ['body'];
+
     public function sender()
     {
         return $this->belongsTo(User::class, 'sender_id');
@@ -25,5 +25,15 @@ class Message extends Model
     public function group()
     {
         return $this->belongsTo(Group::class);
+    }
+
+    public function getBodyAttribute($body)
+    {
+        return decrypt($body);
+    }
+
+    public function setBodyAttribute($value)
+    {
+        $this->attributes['body'] = encrypt($value);
     }
 }
